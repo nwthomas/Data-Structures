b685cb27b7876e7cf004d0878e42d02385a394fb
Nathan Thomas <nwthomas@me.com>
2019-09-14 16:29:40 -0700

    Finish Queue class method construction and make tests pass

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,31 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        """
+        Initializes a new Queue and a size of 0
+        """
+        self.size = 0
+        self.storage = []
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        """
+        Adds an item to the back of the Queue
+        """
+        self.size += 1
+        self.storage = [item] + self.storage
 
-  def len(self):
-    pass
+    def dequeue(self):
+        """
+        Removes an item from the front of the queue
+        and returns it
+        """
+        if self.size is 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.pop(self.size)
+
+    def len(self):
+        """
+        Returns the current length of the Queue
+        """
+        return self.size
