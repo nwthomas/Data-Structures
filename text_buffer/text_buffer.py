edd5f2e2559ef5eb63ce629c49f5da81caa81bbf
Nathan Thomas <nwthomas@me.com>
2019-09-15 23:39:24 -0700

    Complete all required class methods for TextBuffer class

--- text_buffer/text_buffer.py
+++ text_buffer/text_buffer.py
@@ -15,6 +15,38 @@ class TextBuffer:
             for char in init:
                 self.contents.add_to_tail(char)
 
+    def append(self, value):
+        """
+        Adds a new character to the back of the text buffer
+        """
+        for i in range(len(value)):
+            self.contents.add_to_tail(value[i])
+        return self.__str__()
+
+    def prepend(self, value):
+        """
+        Adds a new character to the front of the text buffer
+        """
+        for i in range(len(value) - 1, -1, -1):
+            self.contents.add_to_head(value[i])
+        return self.__str__()
+
+    def delete_back(self, num_delete):
+        """
+        Removes a character from the back of the text buffer
+        """
+        for i in range(num_delete):
+            self.contents.remove_from_tail()
+        return self.__str__()
+
+    def delete_front(self, num_delete):
+        """
+        Remvoes a character from the front of the text buffer
+        """
+        for i in range(num_delete):
+            self.contents.remove_from_head()
+        return self.__str__()
+
     def __str__(self):
         """
         Returns a string to print and replaces private string method in Python
@@ -28,4 +60,8 @@ class TextBuffer:
 
 
 tb = TextBuffer("Hello CS21")
+tb.append(" Dude.")
+tb.delete_front(6)
+tb.prepend("This is nuts. ")
+tb.delete_back(1)
 print(tb)
