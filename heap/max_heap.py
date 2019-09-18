06ee3579193758dd7386b82669f9f5712def8a2b
Nathan Thomas <nwthomas@me.com>
2019-09-18 12:56:33 -0700

    Abstract swap logic into seperate private helper method

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -19,7 +19,6 @@ class Heap:
         Removes the highest value (first array position) and
         sorts the rest of the heap progressively
         """
-        # print(self.storage)
         top = self.storage[0]
         if self.get_size() > 1:
             self.storage[0] = self.storage.pop()
@@ -49,10 +48,7 @@ class Heap:
         """
         p_index = floor((i - 1) / 2)
         while self.storage[i] > self.storage[p_index] and p_index >= 0:
-            self.storage[i], self.storage[p_index] = (
-                self.storage[p_index],
-                self.storage[i],
-            )
+            self._swap(i, p_index)
             i = p_index
             p_index = floor((p_index - 1) / 2)
 
@@ -60,7 +56,6 @@ class Heap:
         """
         Helper function to compare to parent node and and switch if appropriate
         """
-        # print(self.storage)
         # Find left/right indices
         left = (2 * i) + 1
         right = (2 * i) + 2
@@ -76,40 +71,19 @@ class Heap:
             left_compare = self.storage[i] - self.storage[left]
             right_compare = self.storage[i] - self.storage[right]
             if right_compare < left_compare and right_compare < 0:
-                self.storage[i], self.storage[right] = (
-                    self.storage[right],
-                    self.storage[i],
-                )
+                self._swap(i, right)
                 self._sift_down(right)
             elif left_compare <= right_compare and left_compare < 0:
-                self.storage[i], self.storage[left] = (
-                    self.storage[left],
-                    self.storage[i],
-                )
+                self._swap(i, left)
                 self._sift_down(left)
         elif left:
             if self.storage[i] < self.storage[left]:
-                self.storage[i], self.storage[left] = (
-                    self.storage[left],
-                    self.storage[i],
-                )
+                self._swap(i, left)
                 self._sift_down(left)
         elif right:
             if self.storage[i] < self.storage[right]:
-                self.storage[i], self.storage[right] = (
-                    self.storage[right],
-                    self.storage[i],
-                )
+                self._swap(i, right)
                 self._sift_down(right)
 
-
-# l = Heap()
-# l.insert(10)
-# l.insert(5)
-# l.insert(9)
-# l.insert(4)
-# l.insert(7)
-# l.insert(2)
-# l.insert(3)
-# l.insert(12)
-# print(l.delete())
+    def _swap(self, i1, i2):
+        self.storage[i1], self.storage[i2] = self.storage[i2], self.storage[i1]
