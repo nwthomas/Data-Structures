3eb33f124f677aaa4c56883a119b2dfc1589e082
Nathan Thomas <nwthomas@me.com>
2019-09-17 20:20:12 -0700

    Add .get_size() method on Heap class

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,40 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        """
+        Check if stroage is empty, if it is start at beginning
+        Put the value at the end of the list (append)
+        If Arr[i] > Arr[i/2] then swap (parent is bigger)
+        """
+        pass
 
-  def delete(self):
-    pass
+    def delete(self):
+        """
+        """
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        """
+        Return array position 0 on list (since it's a max-heap)
+        """
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        """
+        Return length property of array
+        """
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        """
+        Helper function to compare to parent node and and switch if appropriate
+        """
+        pass
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        """
+        Helper function to compare to parent node and and switch if appropriate
+        """
+        pass
